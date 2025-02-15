f7a5d42afd912da63d1d931a5479448c1d333169
Igor Stravinsky <igorhnogueira@gmail.com>
2025-02-15 16:03:40 -0300

    ajuste no process env

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,9 +2,9 @@
 import "dotenv/config";
 import axios from "axios";
 
-const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3001/api" :"/api"
-// const URL = "https://deploy-jornada-full-stack.onrender.com/api";
+// const { NODE_ENV } = process.env;
+// const URL = NODE_ENV === "development" ? "http://localhost:3001/api" :"/api"
+const URL = "https://spotify-7rd2.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
